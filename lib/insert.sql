672feeacea64b804ee4140087f421c9549b3ca31
Karina Macancela <karymacancela@gmail.com>
2019-09-05 15:29:21 -0400

    creating tables

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO users (name, age) VALUES 
+('Alex', 19),
+('John', 29),
+('Rose', 31),
+('Anthony', 10),
+('Jeff', 20),
+('Lissie', 21),
+('Lila', 29),
+('Carlos', 33),
+('Ben', 40),
+('David', 43),
+('Jean Pierre', 33),
+('Alexander', 22),
+('Ollie', 11),
+('James', 67),
+('Mark', 55),
+('Brett', 30),
+('Yuki', 31),
+('Jessica', 32),
+('Taylor', 33),
+('Theresa', 54);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+('Project 101', 'Science', 100000, '10-20-2018', '10-09-2019')
+;
